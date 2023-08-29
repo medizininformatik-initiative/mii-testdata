447965223d33529ca1c3771066470dec603b7d1a
Julian Gründner <juliangruendner@googlemail.com>
2023-08-29 18:00:53 +0200

    fix update last change date and configure synthea to put all files in one folder

--- synthea/docker-entrypoint.sh
+++ synthea/docker-entrypoint.sh
@@ -9,11 +9,11 @@ RELEVANT_RESOURCES=${RELEVANT_RESOURCES:-"Patient,Encounter,Observation,Conditio
 printf "## Removing previously generated data... \n\n"
 rm -rf output/*
 
-mkdir output/fhir-mii
+mkdir output/fhir-processed
 mkdir output/metadata
 
 printf "\n## Starting continous post processing of synthea data in background... \n\n"
-python3 -u testdata-post-processing.py --log-level info --metadatadir output/metadata --inputdir output/fhir --outputdir output/fhir-mii\
+python3 -u testdata-post-processing.py --log-level info --metadatadir output/metadata --inputdir output/fhir --outputdir output/fhir-processed\
  --timeout $TIMEOUT --gzipfiles $GZIP_OUTPUT_FILES --removeinputfiles $REMOVE_INPUTFILES\
  --relevant-resources $RELEVANT_RESOURCES &
 
